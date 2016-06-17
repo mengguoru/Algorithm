a34b3c8c8344e663ad10a516904a32bb359d7116
mengguoru <mengguoru0@gmail.com>
2016-06-17 13:03:34 +0800

    leetcode 122

---
+++ leetcode/122.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+	int maxProfit(const vector<int>& prices) {
+		int maxPro = 0;
+		
+		for (int i=1;i<prices.size();++i)
+		{
+			if (prices[i] > prices[i - 1])
+				maxPro += prices[i] - prices[i - 1];
+		}
+		return maxPro;
+	}
+};
